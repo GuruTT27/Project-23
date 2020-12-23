7b20b7126fe8a846963bce00d9d45a4b262ae88e
GuruTT27 <75417862+GuruTT27@users.noreply.github.com>
2020-12-23 14:17:42 +0530

    Add files via upload

--- platform.js
+++ platform.js
@@ -5,6 +5,8 @@ class Platform{
    var opt = {isStatic : true}
 
  this.body = Bodies.rectangle(x,y,width,height,opt);
+ this.width = width;
+ this.height = height;
  World.add(world, this.body);
 
 }
--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
 var packageBody,ground;
-var platform1;
+var platform1,platform2,platform3;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -16,8 +16,7 @@ function setup() {
 	createCanvas(800, 700);
 	rectMode(CENTER);
 
-	platform1 = new Platform(375,670,200,30);
-
+	
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
@@ -26,17 +25,21 @@ function setup() {
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
 
-	 groundSprite=createSprite(width/2, 600, width,10);
-	 groundSprite.shapeColor=color(0)
+	//  groundSprite=createSprite(width/2, 600, width,10);
+	//  groundSprite.shapeColor=color(0)
 
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.1, isStatic:true});
+	platform1 = new Platform(375,670,200,20);
+	platform2 = new Platform(285,611,20,100);
+	platform3 = new Platform(465,611,20,100);
+
+	packageBody = Bodies.circle(width/2 , 200 , 20 , {restitution:0, isStatic:true});
 	World.add(world, packageBody);
 
-	ground = Bodies.rectangle(width/2, 600, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
+	// ground = Bodies.rectangle(width/2, 600, width, 10 , {isStatic:true} );
+ 	// World.add(world, ground);
 
 	Engine.run(engine);
   
@@ -46,7 +49,9 @@ function draw() {
   rectMode(CENTER);
   background(0);
 
-  platform1.display(375,670,200,30);
+  platform1.display();
+  platform2.display();
+  platform3.display();
 
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
